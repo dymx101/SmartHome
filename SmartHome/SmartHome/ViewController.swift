7ecd2a43b2dd4f2b6d25a81ebc82075089a9a1ea
dymx101 <dymx101@hotmail.com>
2019-08-06 08:07:59 +0800

    creating new project

---
+++ SmartHome/SmartHome/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by Yiming Dong on 2019/8/6.
+//  Copyright © 2019 Yiming Dong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
