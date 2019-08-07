4be46906f9efc13d5a5b9a94cab12931414bd51e
dymx101 <dymx101@hotmail.com>
2019-08-08 00:07:34 +0800

    add RoomType and FixtureType

---
+++ SmartHome/SmartHome/Data/Models/RoomType.swift
@@ -0,0 +1,15 @@
+//
+//  RoomType.swift
+//  SmartHome
+//
+//  Created by Yiming Dong on 2019/8/7.
+//  Copyright © 2019 Yiming Dong. All rights reserved.
+//
+
+import Foundation
+
+enum RoomType: String {
+    case bedroom = "Bedroom"
+    case livingRoom = "Living Room"
+    case kitcken = "Kitchen"
+}
