da3f68216a0d449281b8383819b83a33987156ea
dymx101 <dymx101@hotmail.com>
2019-08-07 08:29:24 +0800

    add class declaring constants for unit tests

---
+++ SmartHome/SmartHomeTests/UnitTestConstants.swift
@@ -0,0 +1,13 @@
+//
+//  UnitTestConstants.swift
+//  SmartHomeTests
+//
+//  Created by Yiming Dong on 2019/8/7.
+//  Copyright © 2019 Yiming Dong. All rights reserved.
+//
+
+import Foundation
+
+struct UnitTestConstants {
+    static let timeout = 60.0
+}
