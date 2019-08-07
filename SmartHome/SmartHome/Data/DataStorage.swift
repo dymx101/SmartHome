4134d316a25f3b44d14ee518ec16e5556988da37
dymx101 <dymx101@hotmail.com>
2019-08-07 21:59:25 +0800

    reduce file I/O by adding a memory cache object

--- SmartHome/SmartHome/Data/DataStorage.swift
+++ SmartHome/SmartHome/Data/DataStorage.swift
@@ -15,16 +15,35 @@ protocol DataStoring {
 
 class DataStorage: DataStoring {
     private let STORE_KEY_HOUSE = "STORE_KEY_HOUSE"
+    private var houseCache: House?
+    
     func saveHouse(_ house: House?) {
+        guard var house = house else {
+            UserDefaults.standard.set(nil, forKey: STORE_KEY_HOUSE)
+            return
+        }
+        
+        if let houseCache = houseCache {
+            house.rooms.bedroom.fixtureStatusMap = houseCache.rooms.bedroom.fixtureStatusMap
+            house.rooms.livingRoom.fixtureStatusMap = houseCache.rooms.livingRoom.fixtureStatusMap
+            house.rooms.kitcken.fixtureStatusMap = houseCache.rooms.kitcken.fixtureStatusMap
+        }
+        houseCache = house
+        
         let data = try? JSONEncoder().encode(house)
         UserDefaults.standard.set(data, forKey: STORE_KEY_HOUSE)
     }
     
     func loadHouse() -> House? {
+        if houseCache != nil {
+            return houseCache
+        }
+        
         guard let data = UserDefaults.standard.value(forKey: STORE_KEY_HOUSE) as? Data else {
             return nil
         }
         
-        return try? JSONDecoder().decode(House.self, from: data)
+        houseCache = try? JSONDecoder().decode(House.self, from: data)
+        return houseCache
     }
 }
