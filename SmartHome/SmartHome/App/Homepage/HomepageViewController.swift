be6f19eff5d200928e090102689a3a042e06c804
dymx101 <dymx101@hotmail.com>
2019-08-07 22:00:51 +0800

    implement homepage view controller

--- SmartHome/SmartHome/App/Homepage/HomepageViewController.swift
+++ SmartHome/SmartHome/App/Homepage/HomepageViewController.swift
@@ -7,24 +7,35 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Alamofire
 
 class HomepageViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var viewModel: HomepageViewModel!
+    
+    private let sessionManager = SessionManager(configuration: URLSessionConfiguration.default)
+    private var networkManager: NetworkManaging!
+    private var apiService: ApiService!
+    private var dataStorage: DataStorage!
+    
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        networkManager = NetworkManager(sessionManager: sessionManager)
+        apiService = ApiService(networkManager: networkManager)
+        dataStorage = DataStorage()
+        viewModel = HomepageViewModel(apiService: apiService, dataStorage: dataStorage)
+        
+        viewModel.rooms.bind(to: tableView.rx.items(cellIdentifier: "roomCell", cellType: UITableViewCell.self)) { row, model, cell in
+            cell.textLabel?.text = model.name
+        }.disposed(by: disposeBag)
+        
+        viewModel.getRooms()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
