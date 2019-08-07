f758220084cceb2d782fec5f67847d2146c02ae4
dymx101 <dymx101@hotmail.com>
2019-08-07 22:00:19 +0800

    create cell view model for room cell

---
+++ SmartHome/SmartHome/App/Homepage/ViewModels/HomepageCellViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  HomepageCellViewModel.swift
+//  SmartHome
+//
+//  Created by Yiming Dong on 2019/8/7.
+//  Copyright © 2019 Yiming Dong. All rights reserved.
+//
+
+import Foundation
+
+struct HomepageCellViewModel {
+    let name: String
+    let room: Room
+}
